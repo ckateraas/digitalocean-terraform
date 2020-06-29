f690222c886fa79951e603b50b6224dd2a3ac9c7
Christian <user@example.com>
2020-06-29 11:39:31 +0200

    Replace Ubuntu VM with RancherOS VM

--- main.tf
+++ main.tf
@@ -7,14 +7,6 @@ provider "digitalocean" {
   version = "~> 1.19"
 }
 
-module "droplet" {
-  source = "./droplet"
-  image  = var.ubuntu
-  region = var.do_ams3
-  size   = var.price_10_dollars
-  name   = var.droplet_name
-}
-
 module "rancheros" {
   source = "./droplet"
   image  = var.rancheros
@@ -27,15 +19,15 @@ module "dns" {
   source     = "./dns"
   domain     = var.domain
   subdomains = var.subdomains
-  ipv4       = module.droplet.public_ipv4
-  ipv6       = module.droplet.public_ipv6
+  ipv4       = module.rancheros.public_ipv4
+  ipv6       = module.rancheros.public_ipv6
 }
 
 module "firewall" {
   source     = "./firewall"
-  droplet_id = module.droplet.droplet_id
+  droplet_id = module.rancheros.droplet_id
 }
 
 output "ip" {
-  value = module.droplet.public_ipv4
+  value = module.rancheros.public_ipv4
 }
